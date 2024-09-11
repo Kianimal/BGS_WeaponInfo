500d2e914c58488c0fdd911e65f528fbe8cb1dc8
Snapopotamus <spoono17@gmail.com>
2024-09-10 22:15:31 -0600

    added fxmanifest.lua and weapons.ymt files

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+--       ___          ___          ___
+--      /\  \        /\  \        /\  \
+--     /::\  \      /::\  \      /::\  \
+--    /:/\:\  \    /:/\:\  \    /:/\ \  \
+--   /::\~\:\__\  /:/  \:\  \  _\:\~\ \  \
+--  /:/\:\ \:|__|/:/__/_\:\__\/\ \:\ \ \__\
+--  \:\~\:\/:/  /\:\  /\ \/__/\:\ \:\ \/__/
+--   \:\ \::/  /  \:\ \:\__\   \:\ \:\__\
+--    \:\/:/  /    \:\/:/  /    \:\/:/  /
+--     \::/  /      \::/  /      \::/  /
+--      \/__/        \/__/        \/__/
+-- Author: Snapopotamus
+-- © 2024
+---------------------------------------------------------------------------------------------
+
+author "Snapopotamus"
+fx_version "cerulean"
+lua54 "yes"
+
+games { "rdr3" }
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+file 'files/weapons.ymt'
+
+data_file 'WEAPONINFO_FILE' 'files/weapons.ymt'
